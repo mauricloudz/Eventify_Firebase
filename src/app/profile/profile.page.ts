a99e36a68d5711ba51a74b989296d5afd219ffb0
Daniela Castillo <franky1764@gmail.com>
2024-10-10 17:05:44 -0300

    Formulario Profile+Scss+met onsubmit

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
 })
-export class ProfilePage implements OnInit {
+export class ProfilePage {
 
   constructor() { }
 
-  ngOnInit() {
+  // Método para manejar el envío del formulario
+  onSubmit() {
+    // Lógica para manejar el envío de datos del perfil
+    console.log('Perfil actualizado');
+    alert('Cambios guardados con éxito');
   }
-
 }
+
