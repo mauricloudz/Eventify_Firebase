9d9a1200aca8f971e74f610fd2f92f9ff4ad1bf4
Daniela Castillo <franky1764@gmail.com>
2024-10-20 17:07:55 -0300

    Se agrega eventform y ngoninit para la creación de eventos

--- src/app/create-event/create-event.component.ts
+++ src/app/create-event/create-event.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-event',
   templateUrl: './create-event.component.html',
   styleUrls: ['./create-event.component.scss'],
 })
-export class CreateEventComponent {
+export class CreateEventComponent implements OnInit{
+  eventForm: FormGroup = new FormGroup({});
     categories = [
       { name: 'Talleres' },
       { name: 'Desafíos' },
@@ -15,13 +17,31 @@ export class CreateEventComponent {
       { name: 'Stands' },
     ];
 
-  constructor(private modalController: ModalController) { }
+  constructor(
+    private modalController: ModalController,
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit(){
+    this.eventForm = this.formBuilder.group({
+      sede: ['', Validators.required],
+      tipoActividad: ['', Validators.required],
+      tituloEvento: ['', Validators.required],
+      fechaActividad: ['', Validators.required],
+      horarioInicio: ['', Validators.required],
+      horarioTermino: ['', Validators.required],
+    });
+  }
 
   // Método para manejar el envío del formulario
   onSubmit() {
-    console.log('Evento creado');
-    alert('Evento creado con éxito');
-    this.closeModal();
+    if (this.eventForm.valid) {
+      console.log('Evento creado', this.eventForm.value);
+      alert('Evento creado con éxito');
+      this.closeModal();
+    } else {
+      alert('Por favor, completa todos los campos.');
+    }
   }
 
   // Método para cerrar el modal
