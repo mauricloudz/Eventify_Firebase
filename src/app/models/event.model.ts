3ba5d8c7402e4a782647fcaa8e0068b26e1f3b68
Daniela Castillo <franky1764@gmail.com>
2024-10-21 14:38:37 -0300

    interface Event en models event

--- src/app/models/event.model.ts
+++ src/app/models/event.model.ts
@@ -1,5 +1,20 @@
 export interface Event {
     uid:string;
-    asistentes: string;
-    //rellenar campos
+    asistentes: number;
+    asistentesOnline: number;
+    dependencia: string;
+    directorParticipante: string;
+    docenteRepresentante: string;
+    embajadoresVoluntarios: string;
+    enlaceArchivosEvidencia: string;
+    fechaActividad: string;
+    horaInicio: string;
+    horaTermino: String;
+    inscritos: number;
+    invitados: string;
+    lider: string;
+    modalidad: string;
+    sede: string;
+    sublider: string;
+    tipoActividad: String;
 }
